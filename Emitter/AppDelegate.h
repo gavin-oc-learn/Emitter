91bc84b4bfc12f201c9d678ebe443f88e14b2f15
liukai <liukai@jiduo365.cn>
2018-12-04 10:59:28 +0800

    Initial Commit

---
+++ Emitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emitter
+//
+//  Created by liu kai on 2018/12/4.
+//  Copyright © 2018 liu kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
